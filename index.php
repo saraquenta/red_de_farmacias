f1849faf979df511ac1e3a5630aaebaf027b0914
shirley <shirley.arias.mo@gmail.com>
2024-05-12 21:14:00 -0400

    abcd

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php include_once('layouts/header.php'); ?>
 <div class="login-page">
     <div class="text-center">
-       <h1>Bienvenido</h1>
+       <h1>Bienvenido shirley</h1>
        <p>Iniciar sesión </p>
      </div>
      <?php echo display_msg($msg); ?>
